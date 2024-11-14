518e188de06f4653e0176d595d555663b4168b70
itsunknown-2022 <136757633+itsunknown-2022@users.noreply.github.com>
2024-11-14 09:39:06 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const httpProxy = require('http-proxy');
 const proxy = httpProxy.createProxyServer({});
 const port = process.env.PORT || 5000;
 const server = http.createServer((req, res) => {
-  proxy.web(req, res, { target: 'http://194.62.248.12:6077/' });
+  proxy.web(req, res, { target: 'http://37.114.46.140:6126/' });
 });
 server.listen(port, () => {
   console.log(`The server is currently running on port: ${port}`);
